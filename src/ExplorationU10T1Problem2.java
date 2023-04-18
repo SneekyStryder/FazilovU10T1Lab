31b28d15d65797920ce3f57361aa3588cbf2d1f3
Imran Fazilov <ifazilov3584@bths.edu>
2023-04-18 12:27:48 -0400

    period end

--- src/ExplorationU10T1Problem2.java
+++ src/ExplorationU10T1Problem2.java
@@ -5,13 +5,19 @@ public class ExplorationU10T1Problem2
         System.out.println(simpleRecursiveDivision(8));
     }
 
-    // this recursive method returns an int!
     public static int simpleRecursiveDivision(int num)
     {
+        System.out.println("simpleRecursiveDivision called: num = " + num);
         if (num == 0)
         {
+            System.out.println("Base case achieved, returning 0");
             return 0;
         }
-        return num + simpleRecursiveDivision(num / 2);
+        System.out.println("Making recursive call: " + num + " + simpleRecursiveDivision(" + num + " / 2)");
+        int returnVal = num + simpleRecursiveDivision(num / 2);
+
+        System.out.println("Returning " + returnVal);
+        return returnVal;
     }
 }
+
